fb273355182b8f5a1711486d1002ce3302d358fb
Persson-dev <sim16.prib@gmail.com>
2023-11-07 22:28:57 +0100

    disable tests

--- packages/g/glbinding/xmake.lua
+++ packages/g/glbinding/xmake.lua
@@ -22,6 +22,7 @@ package("glbinding")
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DOPTION_BUILD_TESTS=OFF")
         import("package.tools.cmake").install(package, configs)
     end)
 

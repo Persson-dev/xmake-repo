9c3d0e2d996a9be9418ed8136b00f43a7bebbb0c
Simon Pribylski <sim16.prib@gmail.com>
2024-01-02 18:02:53 +0100

    build with own khr-platform

--- packages/g/glbinding/xmake.lua
+++ packages/g/glbinding/xmake.lua
@@ -39,6 +39,8 @@ package("glbinding")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DOPTION_BUILD_TESTS=OFF")
+        table.insert(configs, "-DOPTION_BUILD_OWN_KHR_HEADERS=ON")
+
         import("package.tools.cmake").install(package, configs)
     end)
 

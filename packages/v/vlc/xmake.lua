f093e78594a1194014de813a157664baa4a99f7d
Persson-dev <sim16.prib@gmail.com>
2026-02-22 19:45:33 +0100

    init

---
+++ packages/v/vlc/xmake.lua
@@ -0,0 +1,8 @@
+package("vlc")
+    set_homepage("https://www.videolan.org/vlc/")
+    set_description("VLC media player")
+    set_license("LGPL 2.1")
+
+	add_urls("git.videolan.org/vlc.git")
+
+	add_versions("3.0.0-git", "8d432b090e08f141ce9156dc728afbed4636301b")
